6c48719f23b012ff9e1e5370a5d422b527aff1d8
oneHalo <85913829+oneHalo@users.noreply.github.com>
2023-04-12 03:20:45 -0600

    Update WildlifeScheduleGenerator.java
    Updated WIldLIfe

--- WildlifeScheduleGenerator.java
+++ WildlifeScheduleGenerator.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import javax.swing.*;
 import java.awt.event.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.awt.FlowLayout;
 import java.awt.Panel;
@@ -16,7 +19,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
 
 import java.awt.Component;
 
@@ -61,57 +63,90 @@ public class WildlifeScheduleGenerator extends JFrame {
             forTable.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             forTable.setSize(900, 700);
             forTable.setLocationRelativeTo(null);
-
-            tableModel = new DefaultTableModel(new Object[]{"Hours", "Tasks", "Time Spent", "Time Available"}, 24);
-
-            for (int i = 0; i < 24; i++) {
-                tableModel.setValueAt(i, i, 0);
-                tableModel.setValueAt("", i, 3);
-            }
-            DefaultTableCellRenderer centreRenderer = new DefaultTableCellRenderer();
-            centreRenderer.setHorizontalAlignment(JLabel.CENTER);
-            table = new JTable(tableModel);
-            addToTable(table);
-//            table.setRowHeight(230);
+            addToTable();
+//            tableModel = new DefaultTableModel(new Object[]{"Hours", "Tasks", "Time Spent", "Time Available"}, 24);
 //
+//            for (int i = 0; i < 24; i++) {
+//                tableModel.setValueAt(i, i, 0);
+//                tableModel.setValueAt("", i, 3);
+//            }
+//
+//            table = new JTable(tableModel);
+//            addToTable(table);
+//            table.setRowHeight(230);
+
 //            scrollPane = new JScrollPane(table);
 //            forTable.add(scrollPane, BorderLayout.CENTER);
-//
+
             forTable.setVisible(true);
         }
-        public void addToTable(JTable table){
-
-            var schedule = Schedule.getSchedule();
+        public void addToTable(){
+           var schedule = Schedule.getSchedule();
            var tasks = Schedule.getTasks();
            var animals = Schedule.getAnimals();
-            int p = 0;
-           DefaultTableCellRenderer centre = new DefaultTableCellRenderer();
+            DefaultTableCellRenderer centre = new DefaultTableCellRenderer();
             centre.setHorizontalAlignment(10);
-           for(int i = 0; i< schedule.size(); i++){
-//               JTextArea textArea = new JTextArea();
-//               textArea.setEditable(false);
-//               JScrollPane scrollPane1 = new JScrollPane(textArea);
-//               this.forTable.add(scrollPane1);
-               StringBuilder task = new StringBuilder();
-              //task.append("Time: " + i + "\n");
-               for(int j = 0; j<schedule.get(i).size(); j++){
-                   task.append(tasks.get(schedule.get(i).get(j).getTaskID()-1).getDescription() + " ");
-                  task.append(animals.get(schedule.get(i).get(j).getAnimalID()-1).getName()+"\n");
+//           for(int i = 0; i< schedule.size(); i++){
+//               StringBuilder task = new StringBuilder();
+//               for(int j = 0; j<schedule.get(i).size(); j++){
+//                   task.append(tasks.get(schedule.get(i).get(j).getTaskID()-1).getDescription()+ "\n ");
+//                  task.append(animals.get(schedule.get(i).get(j).getAnimalID()-1).getName()+ "\n ");
+//
+//                }
+//               JPanel temp = new JPanel();
+//               JLabel tempLabel = new JLabel(task.toString());
+//               temp.add(tempLabel);
+//               table.add(temp);
+//
+//            }
+            try {
+                FileWriter myWriter = new FileWriter("schedule.txt");
+                JTextArea textArea = new JTextArea();
+                textArea.setEditable(false);
+                JScrollPane scrollPane1 = new JScrollPane(textArea);
+                this.forTable.add(scrollPane1);
 
-                }JTextArea textArea = new JTextArea();
+//                DefaultTableCellRenderer centre = new DefaultTableCellRenderer();
+//                centre.setHorizontalAlignment(10);
+                for (int i = 0; i < schedule.size(); i++) {
+                    StringBuilder task = new StringBuilder();
+                    task.append("Time: " + i + ":00");
 
-               textArea.append(task.toString() + "\n");
+                    if (backUpVolunteerChecker(schedule.get(i))) {
+                        task.append(" [+ backup volunteer]\n");
+                    } else {
+                        task.append("\n");
+                    }
+                    for (int j = 0; j < schedule.get(i).size(); j++) {
+                        task.append(tasks.get(schedule.get(i).get(j).getTaskID() - 1).getDescription() + " ");
+                        task.append(animals.get(schedule.get(i).get(j).getAnimalID() - 1).getName() + "\n");
 
-               this.table.setValueAt(textArea.toString(), i, 1);
+                    }
+                    task.append("\n");
+                    //JTextArea textArea = new JTextArea();
+                    //textArea.setEditable(false);
+                    textArea.append(task.toString() + "\n");
+                    //JScrollPane scrollPane1 = new JScrollPane(textArea);
+                    myWriter.write(task.toString());
+                    //this.forTable.add(scrollPane1);
+                }myWriter.close();//this.forTable.add(this.table);
 
-
-
-
-            }this.forTable.add(this.table);
-
-
+            }catch (IOException e){
+                System.out.println("Something Went Wrong");
+                e.printStackTrace();
+            }
+        }
+        public boolean backUpVolunteerChecker(ArrayList<Treatment> check){
+            for(int i = 0; i<check.size(); i++){
+                if(check.get(i).getBackupVolunteer()){
+                    return true;
+                }
+            }
+            return false;
         }
     }
+
+
 
     public static void main(String[] args) throws IllegalScheduleException{
         Schedule myJDBC = new Schedule();
@@ -123,6 +158,18 @@ public class WildlifeScheduleGenerator extends JFrame {
         myJDBC.storeGeneralTasks(totalTasks+1);
         myJDBC.fillSchedule();
         myJDBC.fixSchedule();
+
+        var array = myJDBC.fixSchedule();
+        if(array[0] > -1){
+            var schedule = Schedule.getSchedule();
+            var treatment = schedule.get(array[0]).get(array[1]);
+
+            JOptionPane.showMessageDialog(null, "The schedule is conflicting.\n The following treatment has been deleted from the database. Please provide a new starting hour for this treatment in the database OR Proceed without it:\n " + Schedule.getTasks().get(treatment.getTaskID()-1).getDescription() + " - " + Schedule.getAnimals().get(treatment.getAnimalID()-1).getName()
+                    +"\nThe detailed information is: " + "\n AnimalID: " + treatment.getAnimalID() + "\n TaskID: " + treatment.getTaskID() +
+                    "\n StartHour: " + treatment.getStartHour());
+            myJDBC.deleteTreatment(treatment.getAnimalID(), treatment.getTaskID(), treatment.getStartHour());
+            System.exit(1);
+        }
         new WildlifeScheduleGenerator();
     }
 
